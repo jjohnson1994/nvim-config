e41f9babec7d037f8920d545cfbc15c000ed7825
James Johnson <james.09.1994@gmail.com>
2025-05-07 19:47:41 +0100

    fix svelte not registering

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,106 +1,102 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup {
-        ensure_installed = { "vtsls" },
-      }
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "vtsls", "svelte", "prettierd", "eslint_d" },
+				automatic_installation = true,
+			})
 
-      local function on_attach(client, bufnr)
-        if client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-          vim.lsp.inlay_hint.enable(true)
-        end
-      end
+			local lspconfig = require("lspconfig")
 
-      lspconfig.vtsls.setup {
-        on_attach = on_attach,
-        settings = {
-          typescript = {
-            inlayHints = {
-              -- parameterNames = { enabled = "all" },
-              -- parameterTypes = { enabled = true },
-              -- variableTypes = { enabled = true },
-              -- propertyDeclarationTypes = { enabled = true },
-              -- functionLikeReturnTypes = { enabled = true },
-              -- enumMemberValues = { enabled = true },
-            },
-          },
-          javascript = {
-            inlayHints = {
-              -- parameterNames = { enabled = "all" },
-              -- parameterTypes = { enabled = true },
-              -- variableTypes = { enabled = true },
-              -- propertyDeclarationTypes = { enabled = true },
-              -- functionLikeReturnTypes = { enabled = true },
-              -- enumMemberValues = { enabled = true },
-            },
-          },
-        },
-      }
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    keys = {
-      {
-        "K",
-        function()
-          vim.lsp.buf.hover()
-        end,
-        desc = "Hover",
-      },
-      -- {
-      -- 	"<space>ca",
-      -- 	function()
-      -- 		vim.lsp.buf.code_action()
-      -- 	end,
-      -- 	desc = "Code action",
-      -- },
-      {
-        "gK",
-        function()
-          vim.lsp.buf.signature_help()
-        end,
-        desc = "Signature help",
-      },
-      {
-        "gd",
-        function()
-          vim.lsp.buf.definition()
-        end,
-        desc = "Definition",
-      },
-      {
-        "gD",
-        function()
-          vim.lsp.buf.declaration()
-        end,
-        desc = "Declaration",
-      },
-      {
-        "<space>rn",
-        function()
-          vim.lsp.buf.rename()
-        end,
-        desc = "Rename",
-      },
-    },
-  }
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+			local function on_attach(client, bufnr)
+				if client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+					vim.lsp.inlay_hint.enable(true)
+				end
+			end
+
+			lspconfig.vtsls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					typescript = {
+						-- inlayHints = {
+						-- 	parameterNames = { enabled = "all" },
+						-- 	parameterTypes = { enabled = true },
+						-- 	variableTypes = { enabled = true },
+						-- 	propertyDeclarationTypes = { enabled = true },
+						-- 	functionLikeReturnTypes = { enabled = true },
+						-- 	enumMemberValues = { enabled = true },
+						-- },
+					},
+					javascript = {
+						-- inlayHints = {
+						-- 	parameterNames = { enabled = "all" },
+						-- 	parameterTypes = { enabled = true },
+						-- 	variableTypes = { enabled = true },
+						-- 	propertyDeclarationTypes = { enabled = true },
+						-- 	functionLikeReturnTypes = { enabled = true },
+						-- 	enumMemberValues = { enabled = true },
+						-- },
+					},
+				},
+			})
+
+			lspconfig.svelte.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+		end,
+		keys = {
+			{
+				"K",
+				function()
+					vim.lsp.buf.hover()
+				end,
+				desc = "Hover",
+			},
+			-- {
+			-- 	"<space>ca",
+			-- 	function()
+			-- 		vim.lsp.buf.code_action()
+			-- 	end,
+			-- 	desc = "Code action",
+			-- },
+			{
+				"gK",
+				function()
+					vim.lsp.buf.signature_help()
+				end,
+				desc = "Signature help",
+			},
+			{
+				"gd",
+				function()
+					vim.lsp.buf.definition()
+				end,
+				desc = "Definition",
+			},
+			{
+				"gD",
+				function()
+					vim.lsp.buf.declaration()
+				end,
+				desc = "Declaration",
+			},
+			{
+				"<space>rn",
+				function()
+					vim.lsp.buf.rename()
+				end,
+				desc = "Rename",
+			},
+		},
+	},
 }
-
